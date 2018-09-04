008eac2ea81ed4c417739fd8aace3cd07147bb84
jpspyro <jpspyro@gmail.com>
2018-09-04 16:06:26 -0700

    improve name titles, update resume with spacing tweaks, change about me, change font, update font-awesome

--- js/scripts.js
+++ js/scripts.js
@@ -47,7 +47,7 @@
 
         // Add icons to each block
         $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fas fa-code"></i></div>');
+            $(this).prepend('<div class="vtimeline-icon"><i class="fas fa-laptop-code"></i></div>');
         });
 
         // Add dates to the timeline if exists
